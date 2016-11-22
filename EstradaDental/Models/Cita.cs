e7dbbc88c272d057d667aab5f9e61da13249da82
oscar2196 <mercury_zzz9@hotmail.com>
2016-11-22 12:28:22 -0600

    Vi

--- EstradaDental/Models/Cita.cs
+++ EstradaDental/Models/Cita.cs
@@ -40,6 +40,7 @@ namespace EstradaDental.Models
 
 
         //Una cita tiene un cliente
+
         virtual public ApplicationUser cliente { get; set; }
         //Una cita tiene un dentista
         virtual public Doctor doctor { get; set; }
